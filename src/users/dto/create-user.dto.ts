4e937c275e0ea7cd9e816a33e66629eea1da37eb
Phillip Jhon Lumod <philliplumod@gmail.com>
2024-12-23 14:38:25 +0800

    feat: add class-validator for user DTO validation and update schema

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,48 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  first_name: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  middle_initial: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  last_name: string;
+
+  @ApiProperty()
+  @IsString()
+  suffix: string;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  contact_number: string;
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+}
